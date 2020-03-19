ccce3f32a590f4afe6383fb63abc37e974d444e8
leoklems <62294408+leoklems@users.noreply.github.com>
2020-03-19 16:12:29 +0100

    Add files via upload

--- second/settings.py
+++ second/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     
     'accounts1.apps.Accounts1Config',
+    'storages',
 
 ]
 
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
